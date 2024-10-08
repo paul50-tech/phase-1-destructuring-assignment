b4f7ddb6ce3823a46113e3f3eacb942106532afb
Paul Jubilee Owuoth <pauljubilee50@gmail.com>
2024-10-08 07:36:17 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+
+const { blackAndWhite, black, pink } = {
+  blackAndWhite: 'cow',
+  black: 'sheep',
+  pink: 'pig'
+};
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg] = colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +21,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const { muppetName, color, song, job, partner } = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +38,13 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const { 
+  album: { theMuppetMovie: { song2, song4 } }, 
+  nestedJob, 
+  nestedPartner 
+} = nestedMuppet;
+
 
 // Strings
 
